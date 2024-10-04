c8392b728a483bfd339ee317646ba236bb2b4425
Nube28 <159978169+Nube28@users.noreply.github.com>
2024-10-03 21:06:42 -0700

    Forms creadas
    Devez en cuando hago el trabajo
    
    Co-Authored-By: XimenaOA <159595764+XimenaOA@users.noreply.github.com>

---
+++ SeguimientoMedicamento/src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import presentacion.FormIniciarSesion;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FormIniciarSesion formIniciarSesion = new FormIniciarSesion();
+        formIniciarSesion.setVisible(true);
+    }
+    
+}
